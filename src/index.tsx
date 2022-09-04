0cd2174e229c0c6653f2b40a8f8f136044da619c
MarcosRene <renemoura789@gmail.com>
2022-09-04 16:49:36 -0300

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Marcos</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 18,
+    color: '#7159c1',
+  },
+});
+
+export default App;
