0517e5cba0fbca959cb528007da25f4d764ebb07
MarcosRene <renemoura789@gmail.com>
2022-09-04 17:39:02 -0300

    setup deps, theme and fonts

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
 
 const App = () => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Marcos</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <View />;
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 18,
-    color: '#7159c1',
-  },
-});
 
 export default App;
